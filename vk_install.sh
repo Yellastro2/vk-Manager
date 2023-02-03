70faa675dc99e08a7244d5343987429002f38aef
Yellastro2 <77885272+Yellastro2@users.noreply.github.com>
2023-02-03 11:28:49 +0300

    Edit

--- vk_install.sh
+++ vk_install.sh
@@ -2,6 +2,6 @@
 echo "****Installing vk manager****"
 pkg updade
 pkg upgrade
-pkg install python -y
+pkg install python
 curl -o vk_share.py https://raw.githubusercontent.com/Yellastro2/vk-Manager/main/vk_share.py
 python vk_share.py
